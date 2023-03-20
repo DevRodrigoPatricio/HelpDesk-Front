d27f0893a2f751c36e2db5429b75063dfffbd82e
Rodrigo Patricio <rodrigopatricio19@gmail.com>
2023-03-20 16:13:39 -0300

    serviço de mensagem emplementada

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -17,9 +18,13 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(){};
+  constructor(private toast: ToastrService){};
   ngOnInit(): void {}
 
+  logar(){
+    this.toast.error('Usuário ou senha invalido','Login')
+    this.creds.senha= ''
+  }
   ValidaCampos():Boolean {
     if(this.email.valid && this.senha.valid){
       return true;
