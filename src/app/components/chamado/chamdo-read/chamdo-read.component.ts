03c6967036339881292c336a41799649f5668c56
DevRodrigoPatricio <rodrigopatricio19@gmail.com>
2023-03-31 18:42:29 -0300

    componente chamado-read finalizado

--- src/app/components/chamado/chamdo-read/chamdo-read.component.ts
+++ src/app/components/chamado/chamdo-read/chamdo-read.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from './../../../models/chamado';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Cliente } from '../../../models/cliente';
+import { Tecnico } from '../../../models/tecnico';
+import { ClienteService } from '../../../services/cliente.service';
+import { TecnicoService } from '../../../services/tecnico.service';
+import { ChamadoService } from '../../../services/chamado.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chamdo-read',
   templateUrl: './chamdo-read.component.html',
   styleUrls: ['./chamdo-read.component.css']
 })
-export class ChamdoReadComponent {
+export class ChamdoReadComponent implements OnInit {
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: ''
+  }
 
+  constructor(private chamadoService: ChamadoService,
+    private toast: ToastrService,
+    private router: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.chamado.id = this.router.snapshot.paramMap.get('id')
+    this.findById()
+  }
+
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta
+    }, ex => {
+      this.toast.error(ex.error.error)
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MEDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
 }
